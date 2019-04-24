749133594365c1954f3f2dac0486fca4ee38cc0f
Martin Dörfelt <martin.d@andix.de>
2019-04-24 21:54:54 +0200

    improved coding style

--- code/VHDL/sim/cocotb/window_ctrl/window_ctrl_cocotb.py
+++ code/VHDL/sim/cocotb/window_ctrl/window_ctrl_cocotb.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 
+# disable, because assigning values to signals is needed
+# pylint: disable=pointless-statement
+
 from collections import namedtuple
-import math
-import random
 
 import numpy as np
 
@@ -14,7 +15,7 @@ from cocotb.regression import TestFactory
 from cocotb.result import TestFailure
 from cocotb.scoreboard import Scoreboard
 
-from fixfloat import fixedint2ffloat
+import tools_cocotb
 
 
 class WindowMonitor(Monitor):
@@ -22,7 +23,7 @@ class WindowMonitor(Monitor):
     def __init__(self, name, gen, clk, valid, data, callback=None, event=None):
         self.bits_data = gen.bits_data
         self.kernel_size = gen.kernel_size
-        
+
         self.clk = clk
         self.name = name
         self.valid = valid
@@ -34,56 +35,54 @@ class WindowMonitor(Monitor):
         while True:
             yield RisingEdge(self.clk)
             if bool(self.valid.value.integer):
-                print("recv:", self.data.value)
-                print("recv:", self.data, split_slv(self.data.value.integer, self.bits_data, self.bits_data*self.kernel_size*self.kernel_size))
-                print(self.data.value.integer)
-                self._recv(split_slv(self.data.value.integer, self.bits_data, self.bits_data*self.kernel_size*self.kernel_size))
+                window = tools_cocotb.split_slv(
+                    self.data.value.integer, self.bits_data,
+                    self.bits_data*self.kernel_size*self.kernel_size)
+                print("recv:", self.data.value.integer, window)
+                self._recv(window)
 
 
 class WindowModel:
     """Represent a software model of a sliding window."""
     def __init__(self, gen):
-        self.width = gen.width
-        self.height = gen.height
+        self.size = (gen.ch_in, gen.height, gen.width)
         self.stride = gen.stride
         self.kernel_size = gen.kernel_size
         self.ch_in = gen.ch_in
         self.ch_out = gen.ch_out
         self.row = 0
         self.col = 0
-        self.c = 0
         self.expected_output = []
-        self.img_1d = []
-        self.win = []
         self.pos = 0
-        self.data = np.random.randint(2**gen.bits_data, size=(gen.ch_in, gen.height, gen.width))
-    
-        for col in range(0, gen.width-(gen.kernel_size-1), gen.stride):
-            for row in range(0, gen.height-(gen.kernel_size-1), gen.stride):
-                roi = self.data[:, col:col+gen.kernel_size, row:row+gen.kernel_size].reshape(gen.ch_in,-1).tolist()
-                print(roi)
-                self.expected_output.extend(roi * gen.ch_out)
+        self.data = []
+        self.gen = gen
+
+    def generate_new_data(self):
+        self.pos = 0
+        self.data = np.random.randint(2**self.gen.bits_data, size=self.size)
+        new_width = self.gen.width-(self.gen.kernel_size-1)
+        new_height = self.gen.height-(self.gen.kernel_size-1)
+        for col in range(0, new_width, self.gen.stride):
+            for row in range(0, new_height, self.gen.stride):
+                roi = self.data[:, col:col+self.gen.kernel_size,
+                                row:row+self.gen.kernel_size]
+                roi_reshaped = roi.reshape(self.gen.ch_in, -1).tolist()
+                self.expected_output.extend(roi_reshaped * self.gen.ch_out)
         print(self.data)
         print(self.expected_output)
 
-    def next_input(self):
-        next_in = self.data.transpose(1,2,0).flat[self.pos]
+
+    def next_input(self) -> int:
+        next_in = self.data.transpose(1, 2, 0).flat[self.pos]
         self.pos += 1
         return int(next_in)
-
-
-def split_slv(data: int, bitwidth, total_width):
-    data_split = []
-    for i in range(total_width//bitwidth):
-        data, splitted = divmod(data, 2**bitwidth)
-        data_split.append(splitted)
-    return list(reversed(data_split))
 
 
 @cocotb.coroutine
 def run_test(dut):
     """setup testbench and run a test"""
-    generics = namedtuple("generics", ["bits_data", "kernel_size", "stride", "ch_in", "ch_out", "width", "height"])
+    generics = namedtuple("generics", ["bits_data", "kernel_size", "stride",
+                                       "ch_in", "ch_out", "width", "height"])
     gen = generics(dut.C_DATA_TOTAL_BITS.value.integer,
                    dut.C_KSIZE.value.integer,
                    dut.C_STRIDE.value.integer,
@@ -92,37 +91,40 @@ def run_test(dut):
                    dut.C_IMG_WIDTH.value.integer,
                    dut.C_IMG_HEIGHT.value.integer)
 
-    for _ in range(2):
-        # setup monitor, software model and scoreboard
-        output_mon = WindowMonitor("output", gen, dut.isl_clk, dut.osl_valid, dut.oslv_data)
-        window = WindowModel(gen)
-        scoreboard = Scoreboard(dut)
-        scoreboard.add_interface(output_mon, window.expected_output)
+    # setup monitor, software model and scoreboard
+    output_mon = WindowMonitor("output", gen, dut.isl_clk, dut.osl_valid,
+                               dut.oslv_data)
+    window = WindowModel(gen)
+    scoreboard = Scoreboard(dut)
+    scoreboard.add_interface(output_mon, window.expected_output)
 
-        # setup clock
-        clk = Clock(dut.isl_clk, 10, "ns")
-        cocotb.fork(clk.start())
-        
-        # reset/initialize values
+    # setup clock
+    clk = Clock(dut.isl_clk, 10, "ns")
+    cocotb.fork(clk.start())
+
+    # reset/initialize values
+    dut.isl_rst_n <= 0
+    dut.isl_valid <= 0
+    dut.islv_data <= 0
+    yield RisingEdge(dut.isl_clk)
+
+    for _ in range(10):
+        window.generate_new_data()
+
         dut.isl_rst_n <= 0
-        dut.isl_valid <= 0
-        dut.islv_data <= 0
-        yield RisingEdge(dut.isl_clk)
-        dut.isl_rst_n <= 1
         dut.isl_ce <= 1
         dut.isl_get <= 1
+        yield RisingEdge(dut.isl_clk)
+        dut.isl_rst_n <= 1
         yield RisingEdge(dut.isl_clk)
         i = 0
         while i < gen.width:
             j = 0
             while j < gen.height:
-                print(bool(dut.osl_rdy.value.integer), dut.osl_rdy.value.integer)
                 if bool(dut.osl_rdy.value.integer):
-                    print("row, col: ", i, j)
                     for _ in range(gen.ch_in):
                         in_data = window.next_input()
                         print(in_data)
-                        print("generics:", gen.ch_in, gen.kernel_size)
                         dut.isl_valid <= 1
                         dut.islv_data <= in_data
                         yield RisingEdge(dut.isl_clk)
@@ -131,13 +133,13 @@ def run_test(dut):
                 yield RisingEdge(dut.isl_clk)
             i += 1
 
-        if len(window.expected_output) == 0:
+        if window.expected_output == []:
             raise TestFailure("Output is empty.")
-        
+
         for _ in range(50):
             yield RisingEdge(dut.isl_clk)
 
-        raise scoreboard.result
+    raise scoreboard.result
 
 
 def run_tb():
--- code/python_tools/tools_cocotb.py
+++ code/python_tools/tools_cocotb.py
@@ -21,3 +21,13 @@ class GeneralMonitor(Monitor):
             if bool(self.valid.value.integer):
                 print("recv:", self.data.value.integer)
                 self._recv(self.data.value.integer)
+
+
+def split_slv(data: int, bitwidth, total_width) -> list:
+    """Split a "stacked" integer to multiple integers, based on the bitwidth.
+    """
+    data_split = []
+    for _ in range(total_width // bitwidth):
+        data, splitted = divmod(data, 2**bitwidth)
+        data_split.append(splitted)
+    return list(reversed(data_split))
