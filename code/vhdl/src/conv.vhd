8ad811ecfc5c3892cd76adff32720883914f6b68
Martin Dörfelt <martin.d@andix.de>
2020-01-01 13:17:04 +0100

    moved weights BRAM one level higher
    Separating weight and bias BRAM seems to be strange at the first view.
    But the weights are close related to the input data, while the bias is
    close to the output. Thus the both modules are easier to maintain at
    different levels.

--- code/vhdl/src/conv.vhd
+++ code/vhdl/src/conv.vhd
@@ -21,7 +21,6 @@ entity conv is
     C_CH_OUT          : integer range 1 to 512 := 8;
 
     C_KSIZE           : integer range 1 to 3 := 3;
-    STR_WEIGHTS_INIT  : string := "";
     STR_BIAS_INIT     : string := ""
   );
   port (
@@ -30,25 +29,13 @@ entity conv is
     isl_ce        : in std_logic;
     isl_valid     : in std_logic;
     ia_data       : in t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1);
+    ia_weights    : in t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1);
     oslv_data     : out std_logic_vector(C_DATA_TOTAL_BITS-1 downto 0);
     osl_valid     : out std_logic
   );
 end conv;
 
 architecture behavioral of conv is
-  constant C_INT_BITS_DATA : integer range 1 to 16 := C_DATA_TOTAL_BITS-C_DATA_FRAC_BITS_IN;
-
-  -- for BRAM
-  constant C_BRAM_DATA_WIDTH : integer := C_WEIGHTS_TOTAL_BITS*(C_KSIZE*C_KSIZE);
-  constant C_BRAM_SIZE : integer := C_CH_IN*C_CH_OUT;
-  signal usig_addr_cnt : unsigned(log2(C_BRAM_SIZE) - 1 downto 0) := (others => '0');
-  constant C_BRAM_ADDR_WIDTH : integer := usig_addr_cnt'LENGTH;
-  signal slv_ram_weights : std_logic_vector(C_BRAM_DATA_WIDTH-1 downto 0);
-
-  signal usig_addr_cnt_b : unsigned(log2(C_CH_OUT) - 1 downto 0) := (others => '0');
-  constant C_BRAM_ADDR_WIDTH_B : integer := usig_addr_cnt_b'LENGTH;
-  signal slv_ram_bias : std_logic_vector(C_WEIGHTS_TOTAL_BITS-1 downto 0);
-
   -- +log2(C_CH_IN)-1 because all C_CH_IN are summed up -> broaden data width to avoid overflow
   -- new bitwidth = log2(C_CH_IN*(2^old bitwidth-1)) = log2(C_CH_IN) + old bitwidth -> new bw = lb(64) + 8 = 14
   constant C_SUM_TOTAL_BITS : integer range 1 to 32 := C_DATA_TOTAL_BITS+C_WEIGHTS_TOTAL_BITS+1+log2(C_KSIZE-1)*2+log2(C_CH_IN)+C_FIRST_STAGE;
@@ -58,19 +45,21 @@ architecture behavioral of conv is
   -- 1 bit larger than sfix_sum
   signal sfix_sum_bias : sfixed(C_SUM_INT_BITS downto -C_SUM_FRAC_BITS) := (others => '0');
 
-  -- for Convolution
-  signal sl_valid_in_d1 : std_logic := '0';
-  signal a_data_mm_in : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
-  signal a_weights_mm_in : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
-  signal slv_conv_data_out : std_logic_vector(C_SUM_TOTAL_BITS-log2(C_CH_IN)-1 downto 0);
-  signal sl_conv_valid_out : std_logic := '0';
-  signal sl_conv_valid_out_d1 : std_logic := '0';
-  signal sl_conv_valid_out_d2 : std_logic := '0';
+  -- convolution
+  signal slv_mm_data_out : std_logic_vector(C_SUM_TOTAL_BITS-log2(C_CH_IN)-1 downto 0);
+  signal sl_mm_valid_out : std_logic := '0';
+  signal sl_mm_valid_out_d1 : std_logic := '0';
+  signal sl_mm_valid_out_d2 : std_logic := '0';
 
   signal sl_valid_out : std_logic := '0';
   signal sl_valid_out_d1 : std_logic := '0';
   signal slv_data_out : std_logic_vector(C_DATA_TOTAL_BITS-1 downto 0);
   signal int_mm_out_cnt : integer range 0 to C_CH_IN*C_CH_OUT-1 := 0;
+
+  -- bias
+  signal usig_addr_cnt_b : unsigned(log2(C_CH_OUT) - 1 downto 0) := (others => '0');
+  constant C_BRAM_ADDR_WIDTH_B : integer := usig_addr_cnt_b'LENGTH;
+  signal slv_bias : std_logic_vector(C_WEIGHTS_TOTAL_BITS-1 downto 0);
 
   -- debug
   signal int_ch_in_cnt : integer := 0;
@@ -79,28 +68,6 @@ architecture behavioral of conv is
   signal int_pixel_out_cnt : integer := 0;
 
 begin
-  i_bram_weights : entity work.bram
-  generic map(
-    C_DATA_WIDTH  => C_BRAM_DATA_WIDTH,
-    C_ADDR_WIDTH  => C_BRAM_ADDR_WIDTH,
-    C_SIZE        => C_BRAM_SIZE,
-    C_OUTPUT_REG  => 0, -- TODO: check timing
-    STR_INIT      => STR_WEIGHTS_INIT
-  )
-  port map (
-    isl_clk   => isl_clk,
-    isl_en    => '1',
-    isl_we    => '0',
-    islv_addr => std_logic_vector(usig_addr_cnt),
-    islv_data => (others => '0'),
-    oslv_data => slv_ram_weights
-  );
-  gen_array_1d: for i in 0 to C_KSIZE-1 generate
-    gen_array_2d: for j in 0 to C_KSIZE-1 generate
-      a_weights_mm_in(i, j) <= slv_ram_weights(((i+j*C_KSIZE)+1)*C_WEIGHTS_TOTAL_BITS-1 downto (i+j*C_KSIZE)*C_WEIGHTS_TOTAL_BITS);
-    end generate;
-  end generate;
-
   i_bram_bias : entity work.bram
   generic map(
     C_DATA_WIDTH  => C_WEIGHTS_TOTAL_BITS,
@@ -115,7 +82,7 @@ begin
     isl_we    => '0',
     islv_addr => std_logic_vector(usig_addr_cnt_b),
     islv_data => (others => '0'),
-    oslv_data => slv_ram_bias
+    oslv_data => slv_bias
   );
 
   i_mm : entity work.mm
@@ -133,11 +100,11 @@ begin
     isl_clk       => isl_clk,
     isl_rst_n     => isl_rst_n,
     isl_ce        => isl_ce,
-    isl_valid     => sl_valid_in_d1,
-    ia_data       => a_data_mm_in,
-    ia_weights    => a_weights_mm_in,
-    oslv_data     => slv_conv_data_out,
-    osl_valid     => sl_conv_valid_out
+    isl_valid     => isl_valid,
+    ia_data       => ia_data,
+    ia_weights    => ia_weights,
+    oslv_data     => slv_mm_data_out,
+    osl_valid     => sl_mm_valid_out
   );
 
   proc_cnt : process(isl_clk)
@@ -172,27 +139,14 @@ begin
     variable v_sfix_sum : sfixed(C_SUM_INT_BITS-1 downto -C_SUM_FRAC_BITS) := (others => '0');
   begin
     if rising_edge(isl_clk) then
-      if isl_rst_n = '0' then
-        usig_addr_cnt <= (others => '0');
-        usig_addr_cnt_b <= (others => '0');
-      elsif isl_ce = '1' then
-        if isl_valid = '1' then
-          if usig_addr_cnt < C_BRAM_SIZE-1 then
-            usig_addr_cnt <= unsigned(usig_addr_cnt)+1;
-          else
-            usig_addr_cnt <= (others => '0');
-          end if;
-        end if;
-
-        -- wait one cycle for bram data to be available
-        sl_valid_in_d1 <= isl_valid;
-        a_data_mm_in <= ia_data;
-
-        if sl_conv_valid_out = '1' then
+      if isl_ce = '1' then
+        if sl_mm_valid_out = '1' then
           if int_mm_out_cnt < C_CH_IN-1 then
             int_mm_out_cnt <= int_mm_out_cnt+1;
           else
             int_mm_out_cnt <= 0;
+
+            -- bias BRAM addresses depend on output channel
             if usig_addr_cnt_b < C_CH_OUT-1 then
               usig_addr_cnt_b <= unsigned(usig_addr_cnt_b)+1;
             else
@@ -204,27 +158,27 @@ begin
           end if;
           
           v_sfix_sum := resize(
-            v_sfix_sum + to_sfixed(slv_conv_data_out,
-            C_SUM_INT_BITS-log2(C_CH_IN)-1, -C_SUM_FRAC_BITS),
+            v_sfix_sum + to_sfixed(slv_mm_data_out,
+              C_SUM_INT_BITS-log2(C_CH_IN)-1, -C_SUM_FRAC_BITS),
             C_SUM_INT_BITS-1, -C_SUM_FRAC_BITS, fixed_wrap, fixed_truncate);
           sfix_sum <= v_sfix_sum;
         end if;
 
-        if sl_conv_valid_out_d1 = '1' then
-          sfix_sum_bias <= sfix_sum + to_sfixed(slv_ram_bias,
+        if sl_mm_valid_out_d1 = '1' then
+          sfix_sum_bias <= sfix_sum + to_sfixed(slv_bias,
             C_WEIGHTS_TOTAL_BITS-C_WEIGHTS_FRAC_BITS-1, -C_WEIGHTS_FRAC_BITS);
         end if;
 
-        if sl_conv_valid_out_d2 = '1' then
+        if sl_mm_valid_out_d2 = '1' then
           -- resize/round only at this point
           slv_data_out <= to_slv(resize(sfix_sum_bias,
             C_DATA_TOTAL_BITS-C_DATA_FRAC_BITS_OUT-1, -C_DATA_FRAC_BITS_OUT,
             fixed_saturate, fixed_round));
         end if;
 
-        sl_conv_valid_out_d1 <= sl_conv_valid_out;
-        sl_conv_valid_out_d2 <= sl_conv_valid_out_d1;
-        sl_valid_out <= sl_conv_valid_out_d2 when int_mm_out_cnt = 0 and not (C_CH_IN > 1 and sl_valid_out = '1') else '0';
+        sl_mm_valid_out_d1 <= sl_mm_valid_out;
+        sl_mm_valid_out_d2 <= sl_mm_valid_out_d1;
+        sl_valid_out <= sl_mm_valid_out_d2 when int_mm_out_cnt = 0 and not (C_CH_IN > 1 and sl_valid_out = '1') else '0';
         sl_valid_out_d1 <= sl_valid_out;
       end if;
     end if;
--- code/vhdl/src/conv_top.vhd
+++ code/vhdl/src/conv_top.vhd
@@ -1,7 +1,9 @@
 library ieee;
   use ieee.std_logic_1164.all;
+  use ieee.numeric_std.all;
 library util;
   use util.cnn_pkg.all;
+  use util.math_pkg.all;
 
 entity conv_top is
   generic (
@@ -38,8 +40,17 @@ entity conv_top is
 end conv_top;
 
 architecture behavioral of conv_top is
-  signal a_win_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
-  signal slv_win_valid_out : std_logic := '0';
+  -- window control
+  signal a_win_data_out, a_win_data_out_d1 : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
+  signal sl_win_valid_out, sl_win_valid_out_d1 : std_logic := '0';
+
+  -- weights and bias (BRAM)
+  constant C_BRAM_DATA_WIDTH : integer := C_WEIGHTS_TOTAL_BITS*(C_KSIZE*C_KSIZE);
+  constant C_BRAM_SIZE : integer := C_CH_IN*C_CH_OUT;
+  signal usig_addr_cnt : unsigned(log2(C_BRAM_SIZE) - 1 downto 0) := (others => '0');
+  constant C_BRAM_ADDR_WIDTH : integer := usig_addr_cnt'LENGTH;
+  signal slv_weights : std_logic_vector(C_BRAM_DATA_WIDTH-1 downto 0);
+  signal a_weights : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
 
 begin
   i_window_ctrl : entity work.window_ctrl
@@ -62,12 +73,31 @@ begin
     isl_valid => isl_valid,
     islv_data => islv_data,
     oa_data   => a_win_data_out,
-    osl_valid => slv_win_valid_out,
+    osl_valid => sl_win_valid_out,
     osl_rdy   => osl_rdy
   );
 
-  -- TODO: collect all data, i. e. move bram here
-  --       wb and bram data should be available at the same cycle
+  i_bram_weights : entity work.bram
+  generic map(
+    C_DATA_WIDTH  => C_BRAM_DATA_WIDTH,
+    C_ADDR_WIDTH  => C_BRAM_ADDR_WIDTH,
+    C_SIZE        => C_BRAM_SIZE,
+    C_OUTPUT_REG  => 0, -- TODO: check timing
+    STR_INIT      => STR_WEIGHTS_INIT
+  )
+  port map (
+    isl_clk   => isl_clk,
+    isl_en    => '1',
+    isl_we    => '0',
+    islv_addr => std_logic_vector(usig_addr_cnt),
+    islv_data => (others => '0'),
+    oslv_data => slv_weights
+  );
+  gen_array_1d: for i in 0 to C_KSIZE-1 generate
+    gen_array_2d: for j in 0 to C_KSIZE-1 generate
+      a_weights(i, j) <= slv_weights(((i+j*C_KSIZE)+1)*C_WEIGHTS_TOTAL_BITS-1 downto (i+j*C_KSIZE)*C_WEIGHTS_TOTAL_BITS);
+    end generate;
+  end generate;
 
   i_conv : entity work.conv
   generic map (
@@ -82,16 +112,39 @@ begin
     C_KSIZE               => C_KSIZE,
     C_CH_IN               => C_CH_IN,
     C_CH_OUT              => C_CH_OUT,
-    STR_WEIGHTS_INIT      => STR_WEIGHTS_INIT,
     STR_BIAS_INIT         => STR_BIAS_INIT
   )
   port map (
-    isl_clk   => isl_clk,
-    isl_rst_n => isl_rst_n,
-    isl_ce    => isl_ce,
-    isl_valid => slv_win_valid_out,
-    ia_data   => a_win_data_out,
-    oslv_data => oslv_data,
-    osl_valid => osl_valid
+    isl_clk    => isl_clk,
+    isl_rst_n  => isl_rst_n,
+    isl_ce     => isl_ce,
+    isl_valid  => sl_win_valid_out_d1,
+    ia_data    => a_win_data_out_d1,
+    ia_weights => a_weights,
+    oslv_data  => oslv_data,
+    osl_valid  => osl_valid
   );
+
+  proc_data : process(isl_clk)
+  begin
+    if rising_edge(isl_clk) then
+      if isl_rst_n = '0' then
+        usig_addr_cnt <= (others => '0');
+        -- usig_addr_cnt_b <= (others => '0');
+      elsif isl_ce = '1' then
+        -- delay window control data for synchronisation with weight BRAM 
+        a_win_data_out_d1 <= a_win_data_out;
+        sl_win_valid_out_d1 <= sl_win_valid_out;
+
+        -- weight BRAM addresses depend on window control
+        if sl_win_valid_out = '1' then
+          if usig_addr_cnt < C_BRAM_SIZE-1 then
+            usig_addr_cnt <= unsigned(usig_addr_cnt) + 1;
+          else
+            usig_addr_cnt <= (others => '0');
+          end if;
+        end if;
+      end if;
+    end if;
+  end process;
 end behavioral;
