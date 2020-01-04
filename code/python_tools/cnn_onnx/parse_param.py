c6576550ccfb0c07bdfbf27f2b0ec9fe86b5b43a
Martin Dörfelt <martin.d@andix.de>
2020-01-04 18:59:55 +0100

    improved onnx parsing (fixed pylint warnings)

--- code/python_tools/cnn_onnx/parse_param.py
+++ code/python_tools/cnn_onnx/parse_param.py
@@ -3,7 +3,7 @@
 import argparse
 import json
 import math
-from typing import List, Tuple
+from typing import List, Optional, Tuple
 
 import onnx
 from onnx import numpy_helper
@@ -51,108 +51,109 @@ def get_input_shape(net) -> list:
     return [s.dim_value for s in net.graph.input[0].type.tensor_type.shape.dim]
 
 
+class ProcessingElement:
+    """Represents a processing element (PE) in the hardware design.
+    Each PE has to contain exactly one convolution with optional
+    activation function and maximum pooling."""
+    def __init__(self, param):
+        self.relu = 0
+        self.leaky_relu = 0
+        self.pool_param = (0, 0)
+        self.param = param
+
+    def set_activation(self, type_):
+        """Set the activation function of the PE. Possible only once."""
+        assert self.relu == 0 and self.leaky_relu == 0
+        self.relu = 1
+        if type_ == "LeakyRelu":
+            self.leaky_relu = 1
+
+    def set_max_pool(self, ksize, stride):
+        """Set the maximum pooling parameter of the PE. Possible only once."""
+        assert self.pool_param == (0, 0)
+        self.pool_param = (ksize, stride)
+
+    def get_param(self):
+        """Get all parameter of the PE as dict."""
+        self.param.update({
+            "relu": self.relu,
+            "leaky_relu": self.leaky_relu,
+            "pool_kernel": self.pool_param[0],
+            "pool_stride": self.pool_param[1],
+        })
+        return self.param
+
+
 def parse_param(model: str) -> dict:
     """Parse an ONNX model into a python dictionary."""
     net = onnx.load(model)
 
-    relu: List[int] = []
-    leaky_relu: List[int] = []
-    padding: List[int] = []
-    conv_names: List[str] = []
-    conv_kernel: List[int] = []
-    conv_stride: List[int] = []
-    pool_kernel: List[int] = []
-    pool_stride: List[int] = []
-    bitwidth: List[List[int]] = []
-    channel: List[int] = []
+    input_shape = get_input_shape(net)
+    assert input_shape[0] in [1, 3]
+    param_dict = {
+        "channel": [input_shape[0]],
+        "input_height": input_shape[1],
+        "input_width": input_shape[2],
+        "relu": [],
+        "leaky_relu": [],
+        "pad": [],
+        "conv_names": [],
+        "conv_kernel": [],
+        "conv_stride": [],
+        "pool_kernel": [],
+        "pool_stride": [],
+        "bitwidth": [],
+    }
+
+    pes: List[Optional[ProcessingElement]] = []
+    pelem: Optional[ProcessingElement] = None
 
     weights_dict = {}
     for init in net.graph.initializer:
         weights_dict[init.name] = numpy_helper.to_array(init)
 
-    pool_possible = False  # TODO: improve check for missing maxpool
-
-    nodes = net.graph.node
-    for node in nodes:
+    for node in net.graph.node:
         params = parse_node_params(node)
 
         if node.op_type == "QuantizeLinear":
-            scale = int(weights_dict[node.input[1]])
-        elif node.op_type == "Conv":
-            assert False, "Layer not supported"
+            param_dict["scale"] = int(weights_dict[node.input[1]])
         elif node.op_type == "QLinearConv":
-            pad = get_pad(params)
-            padding.append(pad)
+            if pelem:
+                pes.append(pelem)
 
-            conv_names.append(node.input[3].split("_", 1)[0])
-
-            data_bits = 8
-            frac_bits_in, frac_bits_out = (
-                int(data_bits - math.log2(weights_dict[node.input[1]])),
-                int(data_bits - math.log2(weights_dict[node.input[6]])))
-            data_bits_weights = 8
-            frac_bits_weights = (
-                int(data_bits - math.log2(weights_dict[node.input[4]])))
-            bitwidth.append([data_bits, frac_bits_in, frac_bits_out,
-                             data_bits_weights, frac_bits_weights])
-
-            shape = weights_dict[node.input[3]].shape
-            if not channel:
-                # first input channel
-                assert shape[1] in [1, 3]
-                channel.append(shape[1])
-            channel.append(shape[0])
-
-            if pool_possible:
-                assert len(pool_kernel) == len(conv_kernel) - 1, \
-                    "%d %d" % (len(pool_kernel), len(conv_kernel))
-                assert len(pool_stride) == len(conv_stride) - 1, \
-                    "%d %d" % (len(pool_stride), len(conv_stride))
-                pool_kernel.append(0)
-                pool_stride.append(0)
-            else:
-                pool_possible = True
-            ksize, stride = get_kernel_params(params)
-            conv_kernel.append(ksize)
-            conv_stride.append(stride)
+            conv_param = {
+                "conv_names": node.input[3].split("_", 1)[0],
+                "conv_kernel": get_kernel_params(params)[0],
+                "conv_stride": get_kernel_params(params)[1],
+                "channel": weights_dict[node.input[3]].shape[0],
+                "bitwidth": [  # data, frac in, frac out, weight, weight frac
+                    8,
+                    int(8 - math.log2(weights_dict[node.input[1]])),
+                    int(8 - math.log2(weights_dict[node.input[6]])),
+                    8,
+                    int(8 - math.log2(weights_dict[node.input[4]])),
+                ],
+                "pad": get_pad(params),
+            }
+            pelem = ProcessingElement(conv_param)
         elif node.op_type == "GlobalAveragePool":
-            if pool_possible and len(pool_kernel) != len(conv_kernel):
-                assert len(pool_kernel) == len(conv_kernel) - 1
-                assert len(pool_stride) == len(conv_stride) - 1
-                pool_kernel.append(0)
-                pool_stride.append(0)
-            pool_possible = False
+            pes.append(pelem)
         elif node.op_type == "MaxPool":
-            ksize, stride = get_kernel_params(params)
-            pool_kernel.append(ksize)
-            pool_stride.append(stride)
-            pool_possible = False
-        elif node.op_type == "Relu":
-            # TODO: after every Conv layer an activation layer is expected
-            relu.append(1)
-            leaky_relu.append(0)
-        elif node.op_type == "LeakyRelu":
-            relu.append(1)
-            leaky_relu.append(1)
+            assert pelem is not None
+            pelem.set_max_pool(*get_kernel_params(params))
+        elif node.op_type in ["Relu", "LeakyRelu"]:
+            assert pelem is not None
+            pelem.set_activation(node.op_type)
+        else:
+            raise ValueError(
+                "Unknown or unsupported layer type %s" % node.op_type)
 
-    # pe == number of conv layers
-    pelem = len(conv_kernel)
-    _, input_height, input_width = get_input_shape(net)
-    param_dict = {
-        "channel": channel,
-        "input_height": input_height,
-        "input_width": input_width,
-        "scale": scale,
-        "relu": relu,
-        "leaky_relu": leaky_relu,
-        "pad": padding,
-        "conv_names": conv_names,
-        "conv_kernel": conv_kernel,
-        "conv_stride": conv_stride,
-        "pool_kernel": pool_kernel,
-        "pool_stride": pool_stride,
-        "bitwidth": bitwidth,
-        "pe": pelem}
+    # update param dict with data of all pes
+    param_dict["pe"] = len(pes)
+    for current_pe in pes:
+        assert current_pe is not None
+        for key, val in current_pe.get_param().items():
+            param_dict[key].append(val)
     return param_dict
 
 
