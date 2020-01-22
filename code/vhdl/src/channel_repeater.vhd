6e4520ec0505f35a245ef7e3b128ae88aa4f5a6c
Martin Dörfelt <martin.d@andix.de>
2020-01-22 19:59:12 +0100

    refactored window control and channel buffer
    The name channel repeater fits better, because it doesn't buffer, but
    only repeats.
    Select valid windows directly after window buffer, instead of delaying
    it with an repeat signal, which was redundant.

--- code/vhdl/src/channel_repeater.vhd
+++ code/vhdl/src/channel_repeater.vhd
@@ -4,7 +4,7 @@ library ieee;
 library util;
   use util.cnn_pkg.all;
 
-entity channel_buffer is
+entity channel_repeater is
     generic(
     C_DATA_WIDTH  : integer range 1 to 32 := 8;
 
@@ -17,17 +17,15 @@ entity channel_buffer is
     isl_clk     : in std_logic;
     isl_reset   : in std_logic;
     isl_ce      : in std_logic;
-    isl_repeat  : in std_logic;
     isl_valid   : in std_logic;
     ia_data     : in t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1);
     oa_data     : out t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1);
     osl_valid   : out std_logic;
     osl_rdy     : out std_logic
   );
-end channel_buffer;
+end;
 
-architecture behavior of channel_buffer is
-  signal sl_valid_in : std_logic := '0';
+architecture behavior of channel_repeater is
   signal sl_valid_out : std_logic := '0';
   signal int_ch_in_cnt : integer range 0 to C_CH-1 := 0;
   signal int_ch_out_cnt : integer range 0 to C_CH-1 := 0;
@@ -60,7 +58,6 @@ begin
   begin
     if rising_edge(isl_clk) then
       if isl_ce = '1' then
-        sl_valid_in <= isl_valid;
         if isl_valid = '1' then
           sl_valid_out <= '1';
           if int_ch_in_cnt < C_CH-1 then
@@ -71,11 +68,6 @@ begin
         end if;
 
         if sl_valid_out = '1' then
-          if isl_repeat = '0' and sl_valid_in = '0' then
-            -- repeat only when needed, i. e. when the linebuffer is filled
-            sl_valid_out <= '0';
-          end if;
-
           if int_ch_out_cnt < C_CH-1 then
             int_ch_out_cnt <= int_ch_out_cnt+1;
           else
@@ -92,7 +84,7 @@ begin
     end if;
   end process proc_counter;
 
-  osl_rdy <= not (sl_valid_out or sl_valid_in or isl_valid);
+  osl_rdy <= not (sl_valid_out or isl_valid);
   oa_data <= a_ch(0);
   osl_valid <= sl_valid_out;
 end architecture behavior;
--- code/vhdl/src/window_ctrl.vhd
+++ code/vhdl/src/window_ctrl.vhd
@@ -31,17 +31,17 @@ entity window_ctrl is
     osl_valid : out std_logic;
     osl_rdy   : out std_logic
   );
-end window_ctrl;
+end;
 
 architecture behavioral of window_ctrl is
   signal isl_valid_d1 : std_logic := '0';
 
   -- counter
-  signal int_col : integer range 0 to C_IMG_WIDTH := 0;
-  signal int_row : integer range 0 to C_IMG_HEIGHT := 0;
-  signal int_ch_in_cnt : integer range 0 to C_CH_IN := 0;
-  signal int_ch_out_cnt : integer range 0 to C_CH_IN := 0;
-  signal int_repetition_cnt : integer range 0 to C_CH_OUT := 0;
+  signal int_col : integer range 0 to C_IMG_WIDTH-1 := 0;
+  signal int_row : integer range 0 to C_IMG_HEIGHT-1 := 0;
+  signal int_ch_in_cnt : integer range 0 to C_CH_IN-1 := 0;
+  signal int_ch_out_cnt : integer range 0 to C_CH_IN-1 := 0;
+  signal int_repetition_cnt : integer range 0 to C_CH_OUT-1 := 0;
   signal int_pixel_in_cnt : integer range 0 to C_IMG_HEIGHT*C_IMG_WIDTH := 0;
   signal int_pixel_out_cnt : integer range 0 to C_IMG_HEIGHT*C_IMG_WIDTH := 0;
 
@@ -53,21 +53,24 @@ architecture behavioral of window_ctrl is
   signal sl_wb_valid_out : std_logic := '0';
   signal a_wb_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
 
-  -- for channel buffer
-  signal sl_chb_repeat : std_logic := '0';
-  signal sl_chb_valid_in : std_logic := '0';
-  signal sl_chb_valid_in_d1 : std_logic := '0';
-  signal sl_chb_valid_out : std_logic := '0';
-  signal a_chb_data_in : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
-  signal a_chb_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
-  signal sl_chb_rdy : std_logic := '0';
+  -- for selector
+  signal sl_selector_valid_in,
+         sl_selector_valid_out : std_logic := '0';
+  signal a_selector_data_in,
+         a_selector_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
+  signal sl_selector_rdy : std_logic := '0';
+
+  -- for channel repeater
+  signal sl_repeater_valid_out : std_logic := '0';
+  signal a_repeater_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
+  signal sl_repeater_rdy : std_logic := '0';
 
   signal sl_output_valid : std_logic := '0';
   signal a_data_out : t_slv_array_2d(0 to C_KSIZE-1, 0 to C_KSIZE-1) := (others => (others => (others => '0')));
 begin
   gen_window_buffer : if C_KSIZE = 1 generate
-    sl_chb_valid_in <= isl_valid;
-    a_chb_data_in(0, 0) <= islv_data;
+    sl_selector_valid_out <= isl_valid;
+    a_selector_data_out(0, 0) <= islv_data;
   else generate
     -- line buffer
     i_line_buffer : entity work.line_buffer
@@ -104,12 +107,38 @@ begin
       osl_valid   => sl_wb_valid_out
     );
 
-    sl_chb_valid_in <= sl_wb_valid_out;
-    a_chb_data_in <= a_wb_data_out;
+    sl_selector_valid_in <= sl_wb_valid_out;
+    a_selector_data_in <= a_wb_data_out;
+
+    -------------------------------------------------------
+    -- The data is only needed if all of the following conditions are satisfied:
+    --    1. after initial buffering
+    --    2. every C_STRIDE row
+    --    3. every C_STRIDE column
+    --    4. when the window is not shifted at end/start of line
+    -------------------------------------------------------
+    proc_selector: process(isl_clk)
+    begin
+      if rising_edge(isl_clk) then
+        if isl_ce = '1' then
+          if sl_selector_valid_in = '1' and
+              int_pixel_in_cnt >= (C_KSIZE-1)*C_IMG_WIDTH+C_KSIZE-1 and
+              (int_row+1-C_KSIZE+C_STRIDE) mod C_STRIDE = 0 and
+              (int_col+1-C_KSIZE+C_STRIDE) mod C_STRIDE = 0 and
+              int_col+1 > C_KSIZE-1 then
+            sl_selector_valid_out <= '1';
+          else
+            sl_selector_valid_out <= '0';
+          end if;
+
+          a_selector_data_out <= a_selector_data_in;
+        end if;
+      end if;
+    end process;
   end generate;
 
   gen_channel_repeater : if C_CH_OUT > 1 generate
-    -- channel buffer
+    -- channel repeater
     i_channel_repeater : entity work.channel_repeater
     generic map(
       C_DATA_WIDTH  => C_DATA_TOTAL_BITS,
@@ -121,21 +150,20 @@ begin
       isl_clk     => isl_clk,
       isl_reset   => isl_rst_n,
       isl_ce      => isl_ce,
-      isl_repeat  => sl_chb_repeat,
-      isl_valid   => sl_chb_valid_in,
-      ia_data     => a_chb_data_in,
-      oa_data     => a_chb_data_out,
-      osl_valid   => sl_chb_valid_out,
-      osl_rdy     => sl_chb_rdy
+      -- TODO: isl_get??
+      isl_valid   => sl_selector_valid_out,
+      ia_data     => a_selector_data_out,
+      oa_data     => a_repeater_data_out,
+      osl_valid   => sl_repeater_valid_out,
+      osl_rdy     => sl_repeater_rdy
     );
-    sl_chb_repeat <= '1' when int_pixel_in_cnt >= (C_KSIZE-1)*C_IMG_WIDTH+C_KSIZE-1 else '0';
   else generate
-    a_chb_data_out <= a_chb_data_in;
-    sl_chb_valid_out <= sl_chb_valid_in;
-    sl_chb_rdy <= '1';
+    sl_repeater_valid_out <= sl_selector_valid_out;
+    a_repeater_data_out <= a_selector_data_out;
+    sl_repeater_rdy <= '1';
   end generate;
 
-  proc_cnt : process(isl_clk)
+  proc_cnt: process(isl_clk)
   begin
     if rising_edge(isl_clk) then
       if isl_rst_n = '0' then
@@ -149,7 +177,7 @@ begin
         int_row <= 0;
         int_col <= 0;
       elsif isl_ce = '1' then
-        if sl_chb_valid_in_d1 = '1' then
+        if sl_selector_valid_in = '1' then
           if int_ch_in_cnt < C_CH_IN-1 then
             int_ch_in_cnt <= int_ch_in_cnt+1;
           else
@@ -168,6 +196,7 @@ begin
           end if;
         end if;
 
+        -- for debugging
         if osl_valid = '1' then
           if int_ch_out_cnt < C_CH_IN-1 then
             int_ch_out_cnt <= int_ch_out_cnt+1;
@@ -185,38 +214,9 @@ begin
     end if;
   end process proc_cnt;
 
-  -------------------------------------------------------
-  -- Process: Output valid
-  -- The output is valid in the following cases:
-  --    1. after initial buffering
-  --    2. every C_STRIDE row
-  --    3. every C_STRIDE column
-  --    4. when the window is not shifted at end/start of line
-  -------------------------------------------------------
-  proc_output_valid : process(isl_clk)
-  begin
-    if rising_edge(isl_clk) then
-      if isl_ce = '1' then
-        a_data_out <= a_chb_data_out;
-        if ((C_CH_OUT > 1 and sl_chb_valid_in_d1 = '1') or
-            (C_CH_OUT = 1 and sl_chb_valid_in = '1')) and
-            int_pixel_in_cnt >= (C_KSIZE-1)*C_IMG_WIDTH+C_KSIZE-1 and
-            (int_row+1-C_KSIZE+C_STRIDE) mod C_STRIDE = 0 and
-            (int_col+1-C_KSIZE+C_STRIDE) mod C_STRIDE = 0 and
-            int_col+1 > C_KSIZE-1 then
-          sl_output_valid <= '1';
-        elsif int_repetition_cnt = C_CH_OUT-1 and int_ch_out_cnt = C_CH_IN-1 then -- only for ch_in = 1
-          sl_output_valid <= '0';
-        end if;
-
-        sl_chb_valid_in_d1 <= sl_chb_valid_in;
-      end if;
-
-      isl_valid_d1 <= isl_valid;
-    end if;
-  end process proc_output_valid;
-
-  oa_data <= a_data_out;
-  osl_valid <= sl_output_valid;
-  osl_rdy <= isl_get and sl_chb_rdy and not isl_valid_d1;
+  oa_data <= a_repeater_data_out;
+  osl_valid <= sl_repeater_valid_out;
+  -- use sl_lb_valid_out and sl_wb_valid_out to get two less cycles of sl_rdy = '1'
+  -- else too much data would get sent in
+  osl_rdy <= isl_get and sl_repeater_rdy and not (sl_lb_valid_out or sl_wb_valid_out);
 end behavioral;
