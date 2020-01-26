5ac6d61c06804bac82e1217b50193ccabec5dbe1
Martin Dörfelt <martin.d@andix.de>
2020-01-26 12:43:50 +0100

    added doctests for the fixfloat module

--- code/python_tools/fixfloat.py
+++ code/python_tools/fixfloat.py
@@ -7,7 +7,17 @@ import numpy as np
 
 
 def py3round(val: float) -> float:
-    """Get rounding behaviour of python3 in python2 (round to nearest even)."""
+    """Get rounding behaviour of python3 in python2 (round to nearest even).
+    
+    >>> py3round(-1.5)
+    -2.0
+    >>> py3round(-0.5)
+    0.0
+    >>> py3round(0.5)
+    0.0
+    >>> py3round(1.5)
+    2.0
+    """
     if abs(round(val) - val) == 0.5:
         return 2.0 * round(val / 2.0)
     return round(val)
@@ -28,6 +38,24 @@ def float2fixed(number: float, int_bits: int, frac_bits: int) -> str:
             py3round(number * 2**frac_bits)))[2:].zfill(int_bits+frac_bits)
 
 
+def fixed2float(number: str, int_bits: int, frac_bits: int) -> float:
+    """Convert binary signed fixed point to floating point number.
+    
+    >>> fixed2float("1", 1, 0)
+    -1.0
+    >>> fixed2float("0", 1, 0)
+    0.0
+    >>> float2fixed(fixed2float("10101010", 6, 2), 6, 2)
+    '10101010'
+    >>> float2fixed(fixed2float("01010101", 2, 6), 2, 6)
+    '01010101'
+    """
+    if number[0] == "1":
+        return (- 1.0 * (2 ** (int_bits + frac_bits) - int(number, 2)) *
+                2 ** -frac_bits)
+    return float(int(number, 2) * 2 ** -frac_bits)
+
+
 def float2fixedint(number: float, int_bits: int, frac_bits: int) -> int:
     """Convert float to fixed. The representation of the fixed number is an
     unsigned integer of the binary value. Useful if there are only integers as
@@ -42,7 +70,19 @@ def v_float2fixedint(array, int_bits: int, frac_bits: int):
 
 
 def fixedint2ffloat(number: float, int_bits: int, frac_bits: int) -> float:
-    """Convert a binary fixed string to a fixed float number."""
+    """Convert a binary fixed string to a fixed float number.
+    
+    >>> fixedint2ffloat(255, 4, 4)
+    -0.0625
+    >>> fixedint2ffloat(0, 4, 4)
+    0.0
+    >>> float2fixedint(fixedint2ffloat(25, 4, 4), 4, 4)
+    25
+    >>> fixedint2ffloat(-2, 4, 4)
+    Traceback (most recent call last):
+        ...
+    ValueError: invalid literal for int() with base 2: '00000b10'
+    """
     return fixed2float(bin(int(number))[2:].zfill(int_bits + frac_bits),
                        int_bits, frac_bits)
 
@@ -53,19 +93,19 @@ def v_fixedint2ffloat(array, int_bits: int, frac_bits: int):
     return vector_fixedint2ffloat(array, int_bits, frac_bits)
 
 
-def fixed2float(number: str, int_bits: int, frac_bits: int) -> float:
-    """Convert binary signed fixed point to floating point number."""
-    if number[0] == "1":
-        return (- 1.0 * (2 ** (int_bits + frac_bits) - int(number, 2)) *
-                2 ** -frac_bits)
-    return int(number, 2)*2**-frac_bits
-
-
 def float2ffloat(number: float, int_bits: int, frac_bits: int) -> float:
-    """Convert floating point to fixed point number, stored as float."""
-    return max(min(
+    """Convert floating point to fixed point number, stored as float.
+
+    >>> float2ffloat(-5.0, 1, 0)
+    -1.0
+    >>> float2ffloat(5.0, 1, 0)
+    0.0
+    >>> float2ffloat(-0.5, 1, 0)
+    0.0
+    """
+    return float(max(min(
         py3round(number * 2**frac_bits) / 2**frac_bits,
-        2 ** (int_bits - 1) - 2 ** -frac_bits), -2 ** (int_bits - 1))
+        2 ** (int_bits - 1) - 2 ** -frac_bits), -2 ** (int_bits - 1)))
 
 
 def v_float2ffloat(array, int_bits: int, frac_bits: int):
@@ -88,7 +128,17 @@ def float2pow2(number: float, min_exp: int, max_exp: int) -> str:
 
 def random_fixed_array(size: tuple, int_bits: int, frac_bits: int,
                        signed: bool = True):
-    """Generate an array with random fixed point numbers."""
+    """Generate an array with random fixed point numbers.
+    
+    >>> np.amax(random_fixed_array((9, 9, 9), 4, 4)) <= 2**3-2**-4
+    True
+    >>> np.amin(random_fixed_array((9, 9, 9), 4, 4)) >= -2**3
+    True
+    >>> np.amax(random_fixed_array((9, 9, 9), 4, 4, False)) <= 2**4-2**-4
+    True
+    >>> np.amin(random_fixed_array((9, 9, 9), 4, 4, False)) >= 0
+    True
+    """
     arr = np.random.randint(2 ** (int_bits + frac_bits),
                             size=size, dtype=np.int)
     # manually extend the bitwidth to implicitly create unsigned values
@@ -97,7 +147,17 @@ def random_fixed_array(size: tuple, int_bits: int, frac_bits: int,
 
 
 def random_bw(max_bw: int = 16):
-    """Generate a random bitwidth."""
+    """Generate a random bitwidth.
+    
+    >>> 1 <= random_bw()[0] <= 16
+    True
+    >>> 0 <= random_bw()[1] <= 15
+    True
+    >>> 1 <= random_bw(8)[0] <= 8
+    True
+    >>> 0 <= random_bw(8)[1] <= 7
+    True
+    """
     bits = randint(1, max_bw)
     int_bits = randint(1, bits)
     frac_bits = bits - int_bits
