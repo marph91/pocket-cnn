1f26345c6aa354dc2335bd16857e70f3d6d9fb16
Martin Dörfelt <martin.d@andix.de>
2020-03-14 18:46:24 +0100

    refactoring

--- code/python_tools/weights_to_files.py
+++ code/python_tools/weights_to_files.py
@@ -10,32 +10,26 @@ from fixfloat import float2fixed
 
 
 def weights_to_files(kernel, bias, bitwidth: Tuple[int, int],
-                     layer_name: str, output_dir: str) -> None:
+                     layer_name: str, output_dir: str):
     """Write quantized data of weights and bias to files."""
-    os.makedirs(output_dir, exist_ok=True)
-
+    # pylint: disable=too-many-locals
     line_w, line_b, debug_w, debug_b = [], [], [], []
     shape = kernel.shape
-    ch_in = 0
+    ch_in = 1
     for ch_out, item in enumerate(np.nditer(kernel)):
-        sfix = float2fixed(item, *bitwidth)
-
-        line_w.append(str(sfix))
-        debug_w.append(str(item) + " ")
+        line_w.append(str(float2fixed(item, *bitwidth)))
+        debug_w.append(f"{item} ")
         if (ch_out+1) % (shape[2] * shape[3]) == 0:
-            if (ch_in+1) % shape[1] == 0:
-                line_b.append(str(
-                    float2fixed(bias[int((ch_out+1) / (shape[1] * shape[2] *
-                                                       shape[3])-1)],
-                                *bitwidth)))
-                line_b.append("\n")
-                debug_b.append(str(
-                    bias[int((ch_out+1) / (shape[1]*shape[2]*shape[3])-1)]))
-                debug_b.append("\n")
+            if ch_in % shape[1] == 0:
+                bias_index = (ch_out+1) / (shape[1] * shape[2] * shape[3])-1
+                line_b.append(
+                    f"{float2fixed(bias[int(bias_index)], *bitwidth)}\n")
+                debug_b.append(f"{bias[int(bias_index)]}\n")
             line_w.append("\n")
             debug_w.append("\n")
             ch_in += 1
 
+    os.makedirs(output_dir, exist_ok=True)
     for name, data in (("/W_" + layer_name + ".txt", line_w),
                        ("/W_" + layer_name + "_debug.txt", debug_w),
                        ("/B_" + layer_name + ".txt", line_b),
