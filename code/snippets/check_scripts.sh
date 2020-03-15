c6611785074b8b2390d5d6a57bcc66662584554d
Martin Dörfelt <martin.d@andix.de>
2020-03-15 17:20:09 +0100

    limited doctest to the "python_tools" folder
    Other folders don't contain doctests.

--- code/snippets/check_scripts.sh
+++ code/snippets/check_scripts.sh
@@ -10,7 +10,7 @@ set -e
 
 # python checks
 # TODO: fix code in all the run.py files
-find "$ROOT" -name "*.py" -print0 | xargs -0 python3 -m doctest
+find "$ROOT/code/python_tools" -name "*.py" -print0 | xargs -0 python3 -m doctest
 flake8 "$ROOT/code/python_tools"
 MYPYPATH="$ROOT/code/python_tools" mypy "$ROOT/code/python_tools" --config-file "$ROOT/mypy.ini"
 pylint "$ROOT/code/python_tools"
