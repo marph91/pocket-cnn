92ec2755a09d212af8e6b19f5e38fc7dcfe44399
Martin Dörfelt <martin.d@andix.de>
2020-03-15 14:40:18 +0100

    small refactoring of the script checking script

--- code/snippets/check_scripts.sh
+++ code/snippets/check_scripts.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 if [ -z "$1" ]; then
-    echo "Please specify the root drectory."
+    echo "Please specify the root directory."
     exit 1
 fi
 ROOT="$1"
 
 # python checks
+# TODO: fix code in all the run.py files
 find "$ROOT" -name "*.py" -print0 | xargs -0 python3 -m doctest
-pylint "$ROOT/code/python_tools"
 flake8 "$ROOT/code/python_tools"
 MYPYPATH="$ROOT/code/python_tools" mypy "$ROOT/code/python_tools" --config-file "$ROOT/mypy.ini"
+pylint "$ROOT/code/python_tools"
 
 # shell checks
 find "$ROOT" -path "$ROOT/vivado" -prune -o -name "*.sh" -print0 | xargs -0 shellcheck
