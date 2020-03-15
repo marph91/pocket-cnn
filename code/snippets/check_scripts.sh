8392ba95f6e241dec409064cba4a93fb547d7cd8
Martin Dörfelt <martin.d@andix.de>
2020-03-15 17:48:05 +0100

    used binary packages if available

--- code/snippets/check_scripts.sh
+++ code/snippets/check_scripts.sh
@@ -13,7 +13,7 @@ set -e
 find "$ROOT/code/python_tools" -name "*.py" -print0 | xargs -0 python3 -m doctest
 flake8 "$ROOT/code/python_tools"
 MYPYPATH="$ROOT/code/python_tools" mypy "$ROOT/code/python_tools" --config-file "$ROOT/mypy.ini"
-pylint "$ROOT/code/python_tools"
+pylint3 "$ROOT/code/python_tools"
 
 # shell checks
 find "$ROOT" -path "$ROOT/vivado" -prune -o -name "*.sh" -print0 | xargs -0 shellcheck
