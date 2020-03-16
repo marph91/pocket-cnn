408f99e50c39d9fcde6e671da697605eb02d4fbb
Martin Dörfelt <martin.d@andix.de>
2020-03-16 11:28:46 +0100

    added output for easier debugging

--- code/snippets/check_scripts.sh
+++ code/snippets/check_scripts.sh
@@ -10,10 +10,15 @@ set -e
 
 # python checks
 # TODO: fix code in all the run.py files
+echo "doctest"
 find "$ROOT/code/python_tools" -name "*.py" -print0 | xargs -0 python3 -m doctest
+echo "flake8"
 flake8 "$ROOT/code/python_tools"
+echo "mypy"
 MYPYPATH="$ROOT/code/python_tools" mypy "$ROOT/code/python_tools" --config-file "$ROOT/mypy.ini"
+echo "pylint"
 pylint3 "$ROOT/code/python_tools"
 
 # shell checks
+echo "shellcheck"
 find "$ROOT" -path "$ROOT/vivado" -prune -o -name "*.sh" -print0 | xargs -0 shellcheck
